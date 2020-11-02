53e78d7a20360f287dc3307919029db3aa9b4137
phatdroid94 <phatdroid94@gmail.com>
2020-11-02 20:31:55 +0700

    set get'

--- app/src/main/java/com/example/oop05102020/Animal.java
+++ app/src/main/java/com/example/oop05102020/Animal.java
@@ -4,4 +4,15 @@ public class Animal {
     // 1 : Thuộc tính
     String name;
     Integer weight;
+
+    // 2 : Hành vi(Phương thức)
+    public void setName(String name){
+        if (name.length() <= 0){
+            throw new NullPointerException("Chuỗi rỗng");
+        }
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
 }
--- app/src/main/java/com/example/oop05102020/MainActivity.java
+++ app/src/main/java/com/example/oop05102020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.oop05102020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Tạo ra 1 instance (Tạo ra 1 cá thể)
         Animal meo = new Animal();
-        meo.name = "Doremon";
-        meo.weight = 5;
+        meo.setName("");
     }
 }
