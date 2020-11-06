723027eb4fa4e8c45073688fb1ade7bea5313ca5
phatdroid94 <phatdroid94@gmail.com>
2020-11-06 20:52:00 +0700

    ui

--- app/src/main/java/com/example/oop05102020/BaseActivity.java
+++ app/src/main/java/com/example/oop05102020/BaseActivity.java
@@ -1,7 +1,7 @@
 package com.example.oop05102020;
 
 import android.os.Bundle;
-
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +12,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayout());
+    }
+    public void showError(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/oop05102020/MainActivity.java
+++ app/src/main/java/com/example/oop05102020/MainActivity.java
@@ -1,13 +1,5 @@
 package com.example.oop05102020;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
 public class MainActivity extends BaseActivity {
 
 
